286bd15d44d89b2d413abf5a6b3e1a79ee10df97
Kevin Mahoney <716806+kevcube@users.noreply.github.com>
2022-12-11 01:31:30 -0500

    feat: add open source reminder

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,12 @@
-const HelloWorld = () => <h1>Hello, World!</h1>;
-export default HelloWorld;
+import Link from "next/link";
+
+const Home = () => (
+  <div className="home">
+    <h1>Hello World</h1>
+    <Link href="https://github.com/kevcube/icecube.dog">
+      I&apos;m open source
+    </Link>
+  </div>
+);
+
+export default Home;
